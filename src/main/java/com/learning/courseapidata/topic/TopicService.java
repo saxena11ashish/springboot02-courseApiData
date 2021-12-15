dc40905f08cbbedf888c9a3e540fe22009a3416e
saxena11ashish <ashishsxn96@gmail.com>
2021-12-16 00:57:28 +0530

    courseApi with Spring-data (CRUD operations on derby(embedded) DB)

---
+++ src/main/java/com/learning/courseapidata/topic/TopicService.java
@@ -0,0 +1,36 @@
+package com.learning.courseapidata.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service        //in spring, business services are typically singletons
+public class TopicService {
+
+    @Autowired
+    TopicRepository topicRepository;
+
+    public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topicRepository.findById(id).get();
+    }
+
+    public void addTopic(Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void deleteTopic(String id) {
+        topicRepository.delete(getTopic(id));
+    }
+}
